b3036391d2e6b74bccd194dbb945ed8e2acda21c
Connor Zanin <cnnrznn@gmail.com>
2023-01-16 13:11:27 -0700

    Port code from previous tutorial, with util lib calls

--- getting_started/shaders/main.cpp
+++ getting_started/shaders/main.cpp
@@ -1,9 +1,54 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <iostream>
+
 #include "util.h"
 
 int main()
 {
+    GLFWwindow* window = createWindow(1280, 720);
+    
+    uint shaderProgram = compileShaderProgram(
+        "shaders_shaders/vert_shader.vs",
+        "shaders_shaders/frag_shader.fs"
+    );
+
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f,  0.5f, 0.0f
+    };
+
+    unsigned int VAO, VBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    // Load buffer data and assign to attribute index
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    std::cout << "Set buffer data" << std::endl;
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
     return 0;
 }
