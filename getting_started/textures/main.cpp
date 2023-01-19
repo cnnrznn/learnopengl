d4e1867c38ddc7ca94ae87c1047a9f4cebf87c7a
Connor Zanin <cnnrznn@gmail.com>
2023-01-18 21:46:46 -0700

    Halfway t hrough tutorial

--- getting_started/textures/main.cpp
+++ getting_started/textures/main.cpp
@@ -12,7 +12,21 @@ int main()
 
     // load wooden texture
     int width, height, nrChannels;
-    unsigned char *data = stbi_load("container.jpg", &width, &height, &nrChannels, 0); 
+    unsigned char *data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
+    if (data == NULL) {
+        // cleanup and exit
+        return 1;
+    }
+
+    // load image into opengl 2d texture
+    uint texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    stbi_image_free(data);
 
     glfwTerminate();
     return 0;
